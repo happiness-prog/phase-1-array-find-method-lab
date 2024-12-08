7a9247be36399f052c8e78438d4fbb330bca2793
Happiness Maina <mainahappiness361@gmail.com>
2024-12-09 00:29:00 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(season => season.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
